3859996d58c3da72c5e7dd1874f35a603c42bc4d
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-01-30 18:31:29 +0300

    Created file new_1

---
+++ Index_array/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int[] array = {1, 12, 31, 4, 18, 15, 16, 17, 18};
+
+int n = array.Length;
+
+int fing = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == fing)
+    {
+        Console.WriteLine(index);
+        //break;
+    }
+    //index = index +1;
+    index++;
+}
